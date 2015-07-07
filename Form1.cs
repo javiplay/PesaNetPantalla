fc2385cb2f9083f26aa580782781dc64134df0d8
javiplay <j.javierasensio@gmail.com>
2015-07-07 12:40:17 +0200

    Añade referencia web a un servicio de test

--- Form1.cs
+++ Form1.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Drawing;
 using System.Text;
 using System.Windows.Forms;
+using SolucionPesaNetPantalla.lenovo_pc;
 
 namespace SolucionPesaNetPantalla
 {
@@ -14,6 +15,12 @@ namespace SolucionPesaNetPantalla
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            HelloWorldService serv = new HelloWorldService() { Url = "http://192.168.89.100:8000/Test" };
+            MessageBox.Show( serv.SayHello("Javier"));
         }
     }
 }
